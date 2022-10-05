bf62e5580d6038b5f95076cf302a33d921fb69da
dami <dami070mosh@gmail.com>
2022-10-05 09:41:45 +0100

    took off the first character

--- assign.js
+++ assign.js
@@ -1,6 +1,14 @@
-//declare an empty arr
-let arr = []
+//declare an empty array
+let array = []
 
 //declare a variable with your your full name
 
 let fullName = ['dami mosh'] 
+
+//split your full name
+
+let array2 = fullName.split('')
+
+//remove the first and last  letter
+
+let arr3 = arr2.shift()
